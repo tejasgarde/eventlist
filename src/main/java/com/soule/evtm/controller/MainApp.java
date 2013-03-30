118e0e36d703a8da7e761f3f70bd1d5da2d79c26
surajrao <tejasgarde@yahoo.co.in>
2013-03-30 09:02:16 +0530

    initial commit

---
+++ src/main/java/com/soule/evtm/controller/MainApp.java
@@ -0,0 +1,48 @@
+package com.soule.evtm.controller;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp  {
+
+    private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+
+    public static void main(String[] args) throws Exception {
+      ApplicationContext ctx = new ClassPathXmlApplicationContext("/configuration/beans/AppConfig.xml");
+    	ClassLoader cl = ClassLoader.getSystemClassLoader();
+    	 
+        URL[] urls = ((URLClassLoader)cl).getURLs();
+ 
+        for(URL url: urls){
+        	System.out.println(url.getFile());
+        }
+    }
+
+    public void start(Stage stage) throws Exception {
+
+        log.info("Starting Hello JavaFX and Maven demonstration application");
+
+        String fxmlFile = "../../../../../../resources/fxml/hello.fxml";
+        log.debug("Loading FXML for main view from: {}", fxmlFile);
+        FXMLLoader loader = new FXMLLoader();
+        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+
+        log.debug("Showing JFX scene");
+        Scene scene = new Scene(rootNode, 400, 200);
+        scene.getStylesheets().add("/styles/styles.css");
+
+        stage.setTitle("Hello JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
+    }
+}

08dcc960bd77b6380536a4b3a6ff61c88df4ce0e
Tejas Garde <tejasgarde@yahoo.co.in>
2013-04-13 11:52:49 +0530

    Login Page and New App Layout Added

--- src/main/java/com/soule/evtm/controller/MainApp.java
+++ src/main/java/com/soule/evtm/controller/MainApp.java
@@ -8,7 +8,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import org.apache.derby.impl.tools.sysinfo.Main;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -20,7 +23,9 @@ public class MainApp extends Application {
 	
 
     private static final Logger log = LoggerFactory.getLogger(MainApp.class);
-
+    
+    private Stage primaryStage;
+    private BorderPane rootLayout;
     public static void main(String[] args) throws Exception {
     	
     	launch(args);
@@ -37,22 +42,39 @@ public class MainApp extends Application {
 //        }
     }
 
-    private AnchorPane rootLayout;
+    
+    
     public void start(Stage stage) throws Exception {
-
+    		this.primaryStage = stage;
+    		this.primaryStage.setTitle("Event Management");
+    	
         log.info("Starting Hello JavaFX and Maven demonstration application");
 
-        String fxmlFile = "/fxml/events-main.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
-        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource(fxmlFile));
-        rootLayout = (AnchorPane)loader.load();
-
-       
-       
-
-        Scene scene = new Scene(rootLayout);
-        stage.setScene(scene);
-        stage.show();
-        stage.show();
+        try {
+        	
+            String rootFile = "/fxml/RootLayout.fxml";
+            FXMLLoader loader = new FXMLLoader(MainApp.class.getResource(rootFile));
+            rootLayout = (BorderPane)loader.load();
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+        loadApplication();
+    }
+    
+    private void loadApplication(){
+    	String fxmlFile = "/fxml/events-main.fxml";
+    	try {
+			FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxmlFile));
+			AnchorPane mainAppPane = (AnchorPane)loader.load();
+			rootLayout.setCenter(mainAppPane);
+			rootLayout.setBottom(null);
+			rootLayout.setLeft(null);
+			rootLayout.setRight(null);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
     }
 }
